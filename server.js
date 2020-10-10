cbee32f47c2065e1e67015bbf9fab8bd909fbbfb
amandaneal1131 <amandaneal1131@gmail.com>
2020-10-10 13:25:16 -0500

    add files

---
+++ server.js
@@ -0,0 +1,22 @@
+// Dependencies
+const express = require('express');
+
+// Sets up the Express App
+const app = express();
+
+// Sets the port
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Routes
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+// Listener
+app.listen(PORT, function() {
+    console.log(`App is listening on PORT: ${PORT}`);
+});
